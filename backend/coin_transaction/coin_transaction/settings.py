6fa69e86f0058d2f5bbcef851c78bb395aaa87c2
Rai0 <43114095+Rai0@users.noreply.github.com>
2022-05-24 06:36:35 +0300

    change db

--- backend/coin_transaction/coin_transaction/settings.py
+++ backend/coin_transaction/coin_transaction/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': os.environ.get ('DB_NAME'),
         'USER': os.environ.get ('DB_USER'),
         'PASSWORD': os.environ.get ('DB_PASS'),
-        'HOST': 'localhost',
+        'HOST': os.environ.get ('HOST'),
         'PORT': '5432',
         }
 }

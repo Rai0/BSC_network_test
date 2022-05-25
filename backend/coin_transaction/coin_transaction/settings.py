3432be34cac01b1367b0a5bd3d7992f20133be5a
Rai0 <43114095+Rai0@users.noreply.github.com>
2022-05-25 06:49:48 +0300

    ready to deploy on heroku

--- backend/coin_transaction/coin_transaction/settings.py
+++ backend/coin_transaction/coin_transaction/settings.py
@@ -19,7 +19,6 @@ WIFs = os.environ.get ('WIFs')
 my_address = os.environ.get ('MY_ADDR')
 
 # debug setting
-# DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
@@ -119,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'collected/'
+STATIC_URL = '/staticfiles/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 

585565ed72a291e1e120b8b125a50f400b6863c1
Rai0 <43114095+Rai0@users.noreply.github.com>
2022-05-26 02:24:31 +0300

    debug false

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7a8j@5ti)+=bhhc&27+xzctw1y@5ve-l#5duddylpfhuh1tp%1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
